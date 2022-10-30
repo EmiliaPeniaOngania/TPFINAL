c4f301d7138549d22c81f609bd4ff6d2f46cb832
María Emilia Peña Onganía <emilypo1980@gmail.com>
2022-10-30 02:38:35 -0300

    ReentregaV1

--- Proyecto_Final/settings.py
+++ Proyecto_Final/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Proyecto_Final.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "APP_Libreria/templates","chat/templates"],
+        'DIRS': [BASE_DIR / "APP_Libreria/templates",BASE_DIR/"chat/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
